8b63ca362093f37a3fff76bf198a832ea78676ba
ruru <67449714+missa-ruru@users.noreply.github.com>
2020-12-15 14:09:40 +0900

    Add files via upload

---
+++ js/bug_control.js
@@ -0,0 +1,31 @@
+// 入力formのクリック対策
+document.getElementById('user_input1').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input2').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input3').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input4').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input5').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input6').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input7').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input8').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input9').onsubmit = function(){
+    return false;
+}
+document.getElementById('user_input10').onsubmit = function(){
+    return false;
+}
